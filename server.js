9a6a2e8777b383a408cfc96e01521e5b10aa2481
Krystyn Smith <krskrsmith@gmail.com>
2021-12-02 10:38:15 -0600

    added notes for myself. connected mongodb and connected app to the port

--- server.js
+++ server.js
@@ -5,5 +5,17 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+//middleware to parse requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//using the public folder
+app.use(express.static("public"));
+
+//connect to mongo database
+mongoose.connect(process.env.MONGODB)
+
+//attaching the port and the app
+app.listen(PORT, () => {
+    console.log(`Up and running on port ${PORT}`);
+})
