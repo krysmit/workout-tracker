7d4c1b4f9481ee27eff1819a6fab25a782dac2bf
Krystyn Smith <krskrsmith@gmail.com>
2021-12-03 16:09:20 -0600

    added ability to recall workout info

--- routes/api.js
+++ routes/api.js
@@ -11,5 +11,22 @@ router.post("/api/workouts", ({ body }, res) => {
     });
 });
 
+router.get("api/workouts", (req, res) => {
+    db.Workout.find({})
+    .then((data) => {
+        data.forEach((workout) => {
+            var total = 0;
+            workout.exercises.forEach((e) => {
+                total += e.duration;
+            });
+            workout.totalDuration = total;
+        });
+        res.json(data);
+    })
+    .catch((err) => {
+        res.json(err);
+    });
+});
+
 
 module.exports = router;
