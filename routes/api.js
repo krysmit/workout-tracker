505ab22ae8eb259a85046e676b415f7dfd4fa5a4
Krystyn Smith <krskrsmith@gmail.com>
2021-12-03 16:03:38 -0600

    beginning to fill in the api routes to create and edit workouts

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,15 @@
 const router = require("express").Router();
+const db = require("../models");
 
-
+router.post("/api/workouts", ({ body }, res) => {
+    db.Workout.create(body)
+    .then((data) => {
+        res.json(data);
+    })
+    .catch((err) => {
+        res.json(err);
+    });
+});
 
 
 module.exports = router;
