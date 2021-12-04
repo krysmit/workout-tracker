11f331e42cfa754cf58043a615ea97b210f4dbae
Krystyn Smith <krskrsmith@gmail.com>
2021-12-04 14:31:55 -0600

    test to heroku was successful

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const PORT = process.env.PORT || 3000;
-//test
+
 const app = express();
 
 //middleware to parse requests
